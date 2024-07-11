dc5279bd6a274b7b073a7e28a3dc206654a23291
Marvin Céspedes <mcespo@gmail.com>
2024-07-11 10:27:35 -0500

    remove warnings

--- src/libs/swiper.js
+++ src/libs/swiper.js
@@ -1,8 +1,7 @@
 import Swiper from "swiper";
-import { Navigation, FreeMode, Thumbs } from "swiper/modules";
+import { Navigation, Thumbs } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/free-mode";
 import "swiper/css/thumbs";
 
 document.addEventListener("DOMContentLoaded", function () {
